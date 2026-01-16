286411c22bbf5b8b8b9dfdb4dc34538762d2a8f0
227071 <227071@KB37-121-C10>
2026-01-16 13:54:22 +0300

    Добавьте файлы проекта.

---
+++ HouseManagement/AddApplicationPage.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HouseManagement
+{
+    /// <summary>
+    /// Логика взаимодействия для AddApplicationPage.xaml
+    /// </summary>
+    public partial class AddApplicationPage : Page
+    {
+        private Заявки selected;
+        private bool isNew;
+        public AddApplicationPage()
+        {
+            InitializeComponent();
+            selected = new Заявки();
+            isNew = true;
+            SetDataContext();
+        }
+
+        public AddApplicationPage(Заявки selected)
+        {
+            InitializeComponent();
+            this.selected = selected;
+            isNew = false;
+            SetDataContext();
+        }
+
+        public void SetDataContext()
+        {
+            DataContext = selected;
+            ComboBox_Employee.ItemsSource = Emelyanenko_HouseManagementEntities.GetInstance().Сотрудники.ToList();
+            ComboBox_Resident.ItemsSource = Emelyanenko_HouseManagementEntities.GetInstance().Жильцы.ToList();
+            ComboBox_Status.ItemsSource = Emelyanenko_HouseManagementEntities.GetInstance().СтатусыЗаявок.ToList();
+        }
+
+        private void Button_Save_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+            if (String.IsNullOrEmpty(TextBox_Address.Text))
+            {
+                errors.AppendLine("Адрес не заполнен!");
+            }
+            if (ComboBox_Resident.SelectedItem == null)
+            {
+                errors.AppendLine("Жилец не указан!");
+            }
+            if (String.IsNullOrEmpty(TextBox_Phone.Text))
+            {
+                errors.AppendLine("Телефон не заполнен!");
+            }
+            if (String.IsNullOrEmpty(TextBox_Description.Text))
+            {
+                errors.AppendLine("Описание проблемы не заполнено!");
+            }
+            if (ComboBox_Employee.SelectedItem == null)
+            {
+                errors.AppendLine("Ответственный сотрудник не указан!");
+            }
+            if (ComboBox_Status.SelectedItem == null)
+            {
+                errors.AppendLine("Статус заявки не указан!");
+            }
+
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString(), "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            try
+            {
+                if (isNew)
+                {
+                    Emelyanenko_HouseManagementEntities.GetInstance().Заявки.Add(selected);
+                }
+
+                Emelyanenko_HouseManagementEntities.GetInstance().SaveChanges();
+
+                NavigationService.GoBack();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Произошла ошибка при работе с базой данных.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
---
+++ HouseManagement/AllApplicationsPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HouseManagement
+{
+    /// <summary>
+    /// Логика взаимодействия для ApplicationsPage.xaml
+    /// </summary>
+    public partial class ApplicationsPage : Page
+    {
+        public ApplicationsPage()
+        {
+            InitializeComponent();
+            DataGrid_Main.ItemsSource = Emelyanenko_HouseManagementEntities.GetInstance().Заявки.ToList();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            DataGrid_Main.ItemsSource = Emelyanenko_HouseManagementEntities.GetInstance().Заявки.ToList();
+        }
+
+        private void Button_Create_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new AddApplicationPage());
+        }
+
+        private void DataGridRow_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            NavigationService.Navigate(new AddApplicationPage(DataGrid_Main.SelectedItem as Заявки));
+        }
+
+        private void Button_Delete_Click(object sender, RoutedEventArgs e)
+        {
+            if (DataGrid_Main.SelectedItem != null)
+            {
+                if (MessageBox.Show("Вы уверены, что хотите удалить заявку?", "Удаление", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                {
+                    try
+                    {
+                        Emelyanenko_HouseManagementEntities.GetInstance().Заявки.Remove(DataGrid_Main.SelectedItem as Заявки);
+                        Emelyanenko_HouseManagementEntities.GetInstance().SaveChanges();
+                        DataGrid_Main.ItemsSource = Emelyanenko_HouseManagementEntities.GetInstance().Заявки.ToList();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Произошла ошибка при работе с базой данных.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+            }
+        }
+    }
+}
